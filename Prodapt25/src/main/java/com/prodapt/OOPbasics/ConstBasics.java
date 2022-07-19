40aece7a9f9873f00a22b9ea1f029fcdee819068
sachidhanantha.d <sachidhanantha.d@CLAP2958.prodapt.com>
2022-07-19 12:06:13 +0530

    sample

---
+++ Prodapt25/src/main/java/com/prodapt/OOPbasics/ConstBasics.java
@@ -0,0 +1,16 @@
+package com.prodapt.OOPbasics;
+
+public class ConstBasics {
+	 public ConstBasics() {
+	        System.out.println("I m Default Constructor");
+	    }
+	    public ConstBasics(String msg) {
+	        System.out.println("I m Parameterized Constructor");
+	    }
+	    void hi() {
+	        System.out.println("I m normal Method");
+	    }
+	    static void hello() {
+	        System.out.println("I m static Method");
+	    }
+	}
